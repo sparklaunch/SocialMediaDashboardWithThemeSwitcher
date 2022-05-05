778897ec8d8021546949199b18936d801e5fba6d
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-06 07:59:09 +0900

    Initial Commit

---
+++ Shared/SocialMediaDashboardWithThemeSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaDashboardWithThemeSwitcherApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/05/06.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaDashboardWithThemeSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
