18d2d258ac18c61e29ee3ea2ec28f7576e990d74
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-06 09:52:26 +0900

    Fix minor issues.

--- Shared/Views/DashboardComparisonView.swift
+++ Shared/Views/DashboardComparisonView.swift
@@ -9,15 +9,18 @@ import SwiftUI
 
 struct DashboardComparisonView: View {
     let value: Int
+    var isValuePositive: Bool {
+        value >= 0
+    }
     var body: some View {
         HStack {
-            Image(decorative: value >= 0 ? "Up" : "Down")
+            Image(decorative: isValuePositive ? "Up" : "Down")
                 .resizable()
                 .frame(width: 8, height: 4)
             Text("\(abs(value)) Today")
                 .font(.subheadline)
                 .fontWeight(.bold)
-                .foregroundColor(value >= 0 ? Color("PositiveColor") : Color("NegativeColor"))
+                .foregroundColor(isValuePositive ? Color("PositiveColor") : Color("NegativeColor"))
         }
     }
 }
